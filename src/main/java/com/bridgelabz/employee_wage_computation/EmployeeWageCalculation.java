22ab520c8e35fe3adec780a7a1fa79bb29065218
Akshay4955 <akshayshedge11@gmail.com>
2023-02-22 15:57:47 +0530

    Added: Code for Employee is part time

--- src/main/java/com/bridgelabz/employee_wage_computation/EmployeeWageCalculation.java
+++ src/main/java/com/bridgelabz/employee_wage_computation/EmployeeWageCalculation.java
@@ -2,6 +2,7 @@ package com.bridgelabz.employee_wage_computation;
 
 public class EmployeeWageCalculation {
     int IS_FULL_TIME = 1;
+    int IS_PART_TIME = 2;
     int RATE_PER_HOUR = 20;
     int empHours;
 
@@ -12,14 +13,18 @@ public class EmployeeWageCalculation {
     }
 
     void getAttendance() {
-        if (Math.floor(Math.random() * 10) % 2 == IS_FULL_TIME)
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IS_FULL_TIME)
             empHours = 8;
+        else if (empCheck == IS_PART_TIME)
+            empHours = 4;
         else
             empHours = 0;
     }
+
     void dailyWage() {
         int wage = empHours * RATE_PER_HOUR;
-        System.out.println("Employee earned : "+wage);
+        System.out.println("Employee earned : " + wage);
 
     }
 }
