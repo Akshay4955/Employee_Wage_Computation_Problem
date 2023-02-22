e12f94121ea720b74a3b381a2f463fd7c89e3795
Akshay4955 <akshayshedge11@gmail.com>
2023-02-22 15:49:36 +0530

    Added: Code for Employee daily wage calculation

--- src/main/java/com/bridgelabz/employee_wage_computation/EmployeeWageCalculation.java
+++ src/main/java/com/bridgelabz/employee_wage_computation/EmployeeWageCalculation.java
@@ -2,17 +2,24 @@ package com.bridgelabz.employee_wage_computation;
 
 public class EmployeeWageCalculation {
     int IS_FULL_TIME = 1;
+    int RATE_PER_HOUR = 20;
+    int empHours;
 
     public static void main(String[] args) {
         EmployeeWageCalculation employeeWageCalculation = new EmployeeWageCalculation();
         employeeWageCalculation.getAttendance();
+        employeeWageCalculation.dailyWage();
     }
 
     void getAttendance() {
-//        double empCheck = Math.floor(Math.random() * 10) % 2;
         if (Math.floor(Math.random() * 10) % 2 == IS_FULL_TIME)
-            System.out.println("Employee is present");
+            empHours = 8;
         else
-            System.out.println("Employee is absent");
+            empHours = 0;
+    }
+    void dailyWage() {
+        int wage = empHours * RATE_PER_HOUR;
+        System.out.println("Employee earned : "+wage);
+
     }
 }
