449476136870e580344504faf1e659d1a1c6a678
Akshay4955 <akshayshedge11@gmail.com>
2023-02-22 16:24:05 +0530

    Added: Code to calculate monthly wage

--- src/main/java/com/bridgelabz/employee_wage_computation/EmployeeWageCalculation.java
+++ src/main/java/com/bridgelabz/employee_wage_computation/EmployeeWageCalculation.java
@@ -7,27 +7,26 @@ public class EmployeeWageCalculation {
     public static void main(String[] args) {
         EmployeeWageCalculation employeeWageCalculation = new EmployeeWageCalculation();
         employeeWageCalculation.getAttendance();
-        employeeWageCalculation.dailyWage();
     }
 
     void getAttendance() {
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
-            case 1:
-                empHours = 8;
-                break;
-            case 2:
-                empHours = 4;
-                break;
-            default:
-                empHours = 0;
-                break;
+        int totalEmpHours = 0;
+        for (int i = 1; i <= 20; i++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case 1:
+                    empHours = 8;
+                    break;
+                case 2:
+                    empHours = 4;
+                    break;
+                default:
+                    empHours = 0;
+                    break;
+            }
+            totalEmpHours += empHours;
         }
+        System.out.println("Employee earned in month: " + (totalEmpHours * RATE_PER_HOUR));
     }
 
-    void dailyWage() {
-        int wage = empHours * RATE_PER_HOUR;
-        System.out.println("Employee earned : " + wage);
-
-    }
 }
