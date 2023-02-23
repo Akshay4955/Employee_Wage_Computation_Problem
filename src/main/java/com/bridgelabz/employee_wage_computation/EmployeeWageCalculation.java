c6f11b39ec0a8415d7f146841060f48b6a8fcfe5
Akshay4955 <akshayshedge11@gmail.com>
2023-02-23 13:59:29 +0530

    Added: Code to check max working days are 20 or max working hours are 100

--- src/main/java/com/bridgelabz/employee_wage_computation/EmployeeWageCalculation.java
+++ src/main/java/com/bridgelabz/employee_wage_computation/EmployeeWageCalculation.java
@@ -1,8 +1,11 @@
 package com.bridgelabz.employee_wage_computation;
 
 public class EmployeeWageCalculation {
-    int RATE_PER_HOUR = 20;
-    int empHours;
+    public static final int RATE_PER_HOUR = 20;
+    public static final int IS_PART_TIME = 2;
+    public static final int IS_FULL_TIME = 1;
+    public static final int NO_OF_WORKING_DAYS = 20;
+    public static final int MAX_WORKING_HOURS = 100;
 
     public static void main(String[] args) {
         EmployeeWageCalculation employeeWageCalculation = new EmployeeWageCalculation();
@@ -10,23 +13,28 @@ public class EmployeeWageCalculation {
     }
 
     void getAttendance() {
-        int totalEmpHours = 0;
-        for (int i = 1; i <= 20; i++) {
+        int totalEmpHours = 0, totalEmpWage = 0, totalWorkingDays = 0;
+
+        while (totalEmpHours < MAX_WORKING_HOURS && totalWorkingDays < NO_OF_WORKING_DAYS) {
+            int empHours = 0;
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
-                case 1:
+                case IS_FULL_TIME:
                     empHours = 8;
                     break;
-                case 2:
+                case IS_PART_TIME:
                     empHours = 4;
                     break;
                 default:
                     empHours = 0;
                     break;
             }
+            int empWage = empHours * RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            System.out.println("Employee Earned : " + empWage);
             totalEmpHours += empHours;
         }
-        System.out.println("Employee earned in month: " + (totalEmpHours * RATE_PER_HOUR));
+        System.out.println("Employee earned in month: " + totalEmpWage + '\n' + "Employee working hours in month: " + totalEmpHours);
     }
-
 }
