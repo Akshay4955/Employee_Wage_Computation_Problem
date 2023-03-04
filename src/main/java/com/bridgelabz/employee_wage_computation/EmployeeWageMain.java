f739213ee3c028be938237ab2a44781e80dad9f7
Akshay4955 <akshayshedge11@gmail.com>
2023-03-04 19:07:59 +0530

    Refactored: code to save total employee wage with company

--- src/main/java/com/bridgelabz/employee_wage_computation/EmployeeWageMain.java
+++ src/main/java/com/bridgelabz/employee_wage_computation/EmployeeWageMain.java
@@ -4,47 +4,54 @@ public class EmployeeWageMain {
     public static final int IS_PART_TIME = 2;
     public static final int IS_FULL_TIME = 1;
 
+    private final String company;
+    private final int ratePerHour;
+    private final int noOfWorkingDays;
+    private final int maxWorkingHours;
+    private int totalEmpWage;
+
+    public EmployeeWageMain(String company, int ratePerHour, int noOfWorkingDays, int maxWorkingHours) {
+        this.company = company;
+        this.ratePerHour = ratePerHour;
+        this.noOfWorkingDays = noOfWorkingDays;
+        this.maxWorkingHours = maxWorkingHours;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Calculator");
-        EmployeeWageMain employeeWage = new EmployeeWageMain();
-        employeeWage.getMonthlyWage("D-mart", 20,20, 110);
-        System.out.println();
-        employeeWage.getMonthlyWage("Reliance", 17,26, 150);
-        System.out.println();
-        employeeWage.getMonthlyWage("Big-Basket", 15,22, 135);
+        EmployeeWageMain dMart = new EmployeeWageMain("D-mart", 18, 24, 120);
+        EmployeeWageMain reliance = new EmployeeWageMain("Reliance", 20, 20, 100);
+        dMart.getMonthlyWage();
+        System.out.println(dMart);
+        reliance.getMonthlyWage();
+        System.out.println(reliance);
     }
 
 
-    public void getMonthlyWage(String company, int ratePerHour, int noOfWorkingDays, int maxWorkingHours) {
-        int totalEmpHours = 0, totalEmpWage = 0, totalWorkingDays = 0;
+    public void getMonthlyWage() {
+        int totalEmpHours = 0, totalWorkingDays = 0;
         while (totalEmpHours < maxWorkingHours && totalWorkingDays < noOfWorkingDays) {
             totalWorkingDays++;
-            int empHours = getDailyWorkingHours();
-            int empWage = empHours * ratePerHour;
-            totalEmpWage += empWage;
+            int empHours;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHours = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHours = 4;
+                    break;
+                default:
+                    empHours = 0;
+                    break;
+            }
             totalEmpHours += empHours;
         }
-
-        System.out.println("Employee working at " + company + " earned in month: " + totalEmpWage +
-                '\n' + "Employee working hours in month: " + totalEmpHours);
-        System.out.println("Employee Working days in month: " + totalWorkingDays);
+        totalEmpWage = totalEmpHours * ratePerHour;
     }
 
-    public int getDailyWorkingHours() {
-        int empHours;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHours = 8;
-                break;
-            case IS_PART_TIME:
-                empHours = 4;
-                break;
-            default:
-                empHours = 0;
-                break;
+        @Override
+        public String toString() {
+            return "Total employee wage for company " + company + " is " + totalEmpWage;
         }
-
-        return empHours;
     }
-}
